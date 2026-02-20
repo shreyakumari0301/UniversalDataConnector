fb345a3a43abeed2b34d2518ba31711fb8cc23fd
shreyakumari0301 <23f2003785@ds.study.iitm.ac.in>
2026-02-20 20:04:01 +0000

    support tickets and analytics

--- app/models/common.py
+++ app/models/common.py
@@ -1,11 +1,12 @@
-
 from pydantic import BaseModel
-from typing import Any, List
+from typing import Any, List, Optional
+
 
 class Metadata(BaseModel):
     total_results: int
     returned_results: int
     data_freshness: str
+    data_type: Optional[str] = None
 
 class DataResponse(BaseModel):
     data: List[Any]
